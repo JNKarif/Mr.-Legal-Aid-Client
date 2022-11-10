d4d637c94ab4f8af0cf193e3a321c666666c2441
JNKarif <arif.rahman041@gmail.com>
2022-11-10 23:32:53 +0600

    edited

--- src/Pages/Edit/Edit.js
+++ src/Pages/Edit/Edit.js
@@ -8,10 +8,21 @@ const Edit = () => {
 
     const handleEdit = event => {
         event.preventDefault()
-        console.log(user)
+        // console.log(user)
+        fetch(`https://assignment-11-server-five-kappa.vercel.app/edit/${user._id}`,{
+           method:'PUT',
+           headers:{
+            'content-type':'application/josn'
+           },
+           body: JSON.stringify(edit) 
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
     }
 
-    const handleMessage = event => {
+    const handleInputChange = event => {
         const form = event.target.name;
         const value = event.target.value;
         const newMessage = { ...edit };
@@ -22,9 +33,9 @@ const Edit = () => {
     return (
         <div className='text-center'>
 
-            <p className='text-3xl'>Edit Review of Our{user.serviceName} Service </p>
+            <p className='text-3xl'>Edit Review of Our {user.serviceName} Service </p>
             <form onSubmit={handleEdit}>
-                <input onChange={handleMessage} defaultValue={user.message} name='message' type='text' className="textarea m-20 textarea-accent w-3/5" placeholder="Edit your review here"></input>
+                <input onChange={(handleInputChange)} defaultValue={user.message} name='message' type='text' className="textarea m-20 textarea-accent w-3/5" placeholder="Edit your review here"></input>
                 <button type="submit" className='btn btn-outline w-2/5'>Edit</button>
             </form>
 
